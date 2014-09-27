cc7bc21e23ad8fda8535978489c24152b6b39918
Chad Forbes <ninjagamer32@gmail.com>
2014-09-27 11:14:24 -0400

    try 2 stay ali ve

---
+++ CDC14/Assets/Scripts/HIVCollision.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HIVCollision : MonoBehaviour {
+	float speed = 1f;//150
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rigidbody2D.AddForce(new Vector2((-speed),0));
+	}
+
+	void OnCollisionEnter2D(Collision2D col){
+		if (col.gameObject.name == "Player") {
+			Destroy (gameObject);
+			//will remove when new interaction is placed
+			Debug.Log ("Been hit");
+		}
+		if (col.gameObject.name == "Antibody") {
+			Destroy (gameObject);
+			Debug.Log ("enemy destroy");
+
+			Destroy (col.gameObject);
+		}
+	}
+}
