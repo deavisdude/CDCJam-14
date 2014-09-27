6d2ce0763182d790594ef714dae19048c6b7bd24
Davis Odom <deavisdude@gmail.com>
2014-09-26 23:17:42 -0400

    Movement is ready, but I think that following is going to be an upgrade

---
+++ CDC14/Assets/Scripts/BFollow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BFollow : MonoBehaviour {
+
+
+	void Start () {
+		
+	}
+	
+
+	void Update () {
+		for(int i=0; i<MoveT.delayPos.Count; i++){
+			MoveT.PosOb poso = (MoveT.PosOb) MoveT.delayPos[i];
+			if((Time.time - poso.time) <= 0.3f && (Time.time - poso.time) >= 0.25f){
+				transform.position = poso.position;
+				MoveT.delayPos.Remove(poso);
+			}
+		}
+	}
+}
