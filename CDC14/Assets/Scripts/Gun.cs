49973d3c3a9d91947c0723eab2260eb3ba7a3956
Davis Odom <deavisdude@gmail.com>
2014-09-26 23:48:53 -0400

    Added Shooting

---
+++ CDC14/Assets/Scripts/Gun.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gun : MonoBehaviour {
+
+	public GameObject projectile;
+	float elapsedT = 0f;
+	public float shootSpeed = 0.1f;
+
+	void Update () {
+		if(Input.GetButton("Fire1") && elapsedT >= shootSpeed){
+			GameObject go = (GameObject)Instantiate(projectile, transform.position, transform.rotation);
+			go.rigidbody2D.AddForce(new Vector2(1000f, 0f));
+			elapsedT = 0f;
+		}
+		elapsedT += Time.deltaTime;
+	}
+}
