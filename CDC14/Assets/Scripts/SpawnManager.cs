38fddea6ab946d949b8ebc48485e8eb3164d2cb6
Davis Odom <deavisdude@gmail.com>
2014-09-27 10:54:53 -0400

    Working on spawns

--- CDC14/Assets/Scripts/SpawnManager.cs
+++ CDC14/Assets/Scripts/SpawnManager.cs
@@ -3,12 +3,42 @@ using System.Collections;
 
 public class SpawnManager : MonoBehaviour {
 
-	// Use this for initialization
+	public class Enemy{
+		string Type;
+		GameObject GOB;
+
+		public enum types  {HIV, EarlyInfected, LateInfected, ForeignParticle};
+
+		public Enemy(types type, GameObject gameOB){
+			Type = type.ToString();
+			GOB = gameOB;
+		}
+
+		void GenDir(){
+			GOB.rigidbody2D.AddForce((GOB.transform.position - GameObject.Find("Player").transform.position)*500f);
+		}
+	}
+
+	public static IList spawnQueue;
+	public int enemyMultiplier;
+	public GameObject[] prefabs;
+
 	void Start () {
-	
+		enemyMultiplier = Application.levelCount*10;
+		for(int i=0; i<enemyMultiplier; i++){
+			spawnQueue.Add(new Enemy(Enemy.types.HIV, Send())); 
+		}
 	}
 	
-	// Update is called once per frame
+	GameObject Send(){
+		GameObject spawner;
+		if(Random.value <0.5f) spawner = GameObject.Find("Spawn1");
+		else spawner = GameObject.Find("Spawn2");
+
+		GameObject go = (GameObject) Instantiate(prefabs[0], spawner.transform.position, spawner.transform.rotation);
+		return go;
+	}
+
 	void Update () {
 	
 	}
