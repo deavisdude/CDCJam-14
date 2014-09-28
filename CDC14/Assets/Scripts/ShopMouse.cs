31cd38c9f7630cb516b959d173471bb246ef9d9b
Chad Forbes <ninjagamer32@gmail.com>
2014-09-28 11:12:46 -0400

    tesr

---
+++ CDC14/Assets/Scripts/ShopMouse.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShopMouse : MonoBehaviour {
+	public Texture texture;
+
+	// Use this for initialization
+	void OnMouseEnter(){
+		gameObject.GetComponent<GUITexture>().texture = texture;
+		Debug.Log("Test");
+	}
+	
+	// Update is called once per frame
+	void OnMouseExit(){
+		gameObject.GetComponent<GUITexture>().texture = null;
+
+	}
+
+	void OnMouseDown(){
+
+	}
+}
