243c532319f6b4e9e22e765ce36a3224c9dcf446
Davis Odom <deavisdude@gmail.com>
2014-09-29 19:41:48 -0400

    Level Borders Fixed!

--- CDC14/Assets/Scripts/Player.cs
+++ CDC14/Assets/Scripts/Player.cs
@@ -24,8 +24,21 @@ public class Player : MonoBehaviour {
 	public float speed = 5;
 	Vector3 acc;
 
+	float dist;
+	float leftLimitation;
+	float rightLimitation;
+	float upLimitation;
+	float downLimitation;
+
 	void Start(){
 		acc = new Vector3();
+
+		dist = (transform.position.z - Camera.main.transform.position.z);
+		leftLimitation = Camera.main.ViewportToWorldPoint(new Vector3(0.03f,0,dist)).x;
+		rightLimitation = Camera.main.ViewportToWorldPoint(new Vector3(.97f,0,dist)).x;
+		upLimitation = Camera.main.ViewportToWorldPoint(new Vector3(0,.97f,dist)).y;
+		downLimitation = Camera.main.ViewportToWorldPoint(new Vector3(0,.03f,dist)).y;
+
 	}
 
 	void CycleWeapon(){
@@ -42,6 +55,13 @@ public class Player : MonoBehaviour {
 	void Update () {
 		acc = new Vector3(Input.GetAxis("Horizontal")*speed*Time.deltaTime, Input.GetAxis("Vertical")*speed*Time.deltaTime,0);
 		transform.position += acc;
+		Vector3 pos = transform.position;
+
+		pos.x = Mathf.Clamp(pos.x, leftLimitation, rightLimitation);
+		pos.y = Mathf.Clamp(pos.y, downLimitation, upLimitation);
+
+		transform.position = pos;
+
 		Vector3 newPos = new Vector3(transform.position.x+1.5f, transform.position.y, 0);
 		delayPos.Add(new PosOb(newPos, Time.time));
 
@@ -54,6 +74,12 @@ public class Player : MonoBehaviour {
 		if(col.gameObject.tag != "Good" && col.gameObject.tag != "Antibody" && col.gameObject.tag != "Border"){
 			Destroy(gameObject);
 			GameObject.Find("AudioManager").GetComponent<AudioManager>().PlayPlayerDeath();
+		}
+	}
+
+	void OnTriggerEnter2D(Collision2D col){
+		if(col.gameObject.tag == "Border"){
+			Debug.Log("HitWall");
 		}
 	}
 
