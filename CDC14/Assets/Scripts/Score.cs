e5720a03e5c20f316c04f3b09d4adec27c83f536
Chad Forbes <ninjagamer32@gmail.com>
2014-09-29 17:37:51 -0400

    add script to Emtricitabine

---
+++ CDC14/Assets/Scripts/Score.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Score : MonoBehaviour {
+	TextMesh texts;
+
+	// Use this for initialization
+	void Start () {
+		texts = gameObject.GetComponent<TextMesh>();
+		Font Arial = (Font)Resources.GetBuiltinResource (typeof(Font), "Arial.ttf");
+		float pixelRatio = (Camera.main.orthographicSize * 2.0f) / Camera.main.pixelHeight;
+		texts.transform.localScale = new Vector3(pixelRatio * 10.0f, pixelRatio * 10.0f, pixelRatio * 0.1f);;
+		texts.text = "Current balance";
+		texts.font = Arial;
+		texts.fontSize = 17;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
