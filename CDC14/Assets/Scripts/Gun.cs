2ad1d0425c99c0bb48344302c6b96822668a4cb6
Davis Odom <deavisdude@gmail.com>
2014-11-15 17:18:35 -0500

    Added a muzzle flash

--- CDC14/Assets/Scripts/Gun.cs
+++ CDC14/Assets/Scripts/Gun.cs
@@ -8,11 +8,15 @@ public class Gun : MonoBehaviour {
 	public float shootSpeed = 0.05f;
 
 	void Update () {
+		if(GameObject.Find("Muzzle Flash").GetComponent<SpriteRenderer>().enabled){
+			GameObject.Find("Muzzle Flash").GetComponent<SpriteRenderer>().enabled = false;
+		}
 
 		if (Application.loadedLevelName != "Menu" && !Player.dead) {
 			if (Input.GetButton ("Fire1") && elapsedT >= shootSpeed) {//!pause.activeSelf
 				GameObject go = (GameObject)Instantiate (projectile, new Vector3 (transform.position.x, transform.position.y, transform.position.z + 1), transform.rotation);
 				float miss =Random.Range(-.04f,.04f);
+				GameObject.Find("Muzzle Flash").GetComponent<SpriteRenderer>().enabled = true;
 				go.transform.localRotation = new Quaternion(go.transform.localRotation.x,go.transform.localRotation.y,go.transform.localRotation.z+miss,go.transform.localRotation.w);
 				go.rigidbody2D.AddRelativeForce (new Vector2 (1000f, 0f));
 				GameObject.Find ("AudioManager").GetComponent<AudioManager> ().PlayPewPew ();
